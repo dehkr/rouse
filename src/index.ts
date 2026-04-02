36b0892e2a684383628612b812b2fd39b0ed43ca
dehkr <dehkr25@gmail.com>
2026-04-02 09:25:13 -0500

    Remove bus and XHR support and update exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import { Rouse } from './core';
-
 export { createApp } from './core/app';
+export { debounce, throttle } from './core/timing';
 export { controller } from './dom/controller';
+export { dispatch, on } from './dom/utils';
 export { computed, effect, reactive, signal, trigger } from './reactivity';
-export type { BindableValue, SetupContext, SetupFn } from './types';
-export { Rouse };
+export type { BindableValue, LifecycleEvent, SetupContext, SetupFunction } from './types';
