01619b081fc95901ca68d2d86e418121fc07d3b1
dehkr <dehkr25@gmail.com>
2026-04-03 21:46:28 -0500

    Export 'insert' utility

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { createApp } from './core/app';
 export { debounce, throttle } from './core/timing';
 export { controller } from './dom/controller';
-export { dispatch, on } from './dom/utils';
+export { dispatch, insert, on } from './dom/utils';
 export { computed, effect, reactive, signal, trigger } from './reactivity';
 export type { BindableValue, LifecycleEvent, SetupContext, SetupFunction } from './types';
+
